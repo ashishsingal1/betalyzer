44ffb6aea8e25c563d5c702b0a6734edf9354a69
asingal <ashish.singal1@gmail.com>
2017-01-21 16:54:33 -0500

    init

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template
+from bokeh.embed import components
+from bokeh.charts import Line
+
+import betalyzer
 
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-	return render_template('index.html')
+    line = Line(betalyzer.df_betas)
+    bokeh_script, bokeh_div = components(line)
+    return render_template('index.html', dt_tickers=betalyzer.df_tickers.to_dict(orient='records'),
+                           bokeh_script=bokeh_script, bokeh_div=bokeh_div)
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
---
+++ betalyzer.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+df_betas = pd.read_pickle('df_beta.pkl')
+df_tickers = pd.read_pickle('df_tickers.pkl')
+
+def recalculate():
+    pass
